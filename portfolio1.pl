5b39ff6e15dedbe216dc53f4247e7e5d1c66dc87
Sukhoroslov <asukhoroslov@york.citycollege.eu>
2025-10-20 17:33:58 +0300

    Add file facts to portfolio1.pl

--- portfolio1.pl
+++ portfolio1.pl
@@ -19,6 +19,17 @@ folder(cat3_2_4_3).
 folder(subcat3_2_4_2_1).
 folder(subpr3_3_1).
 
+file(presentation1, pptx, 133, date(25,10,2023)).
+file(presentation2, pptx, 148, date(25,10,2023)).
+file(lab1, pl, 1, date(1,11,2023)).
+file(lab2, pl, 1, date(25,10,2023)).
+file(lab3, pl, 1, date(16,11,2023)).
+file(lab4, pl, 1, date(8,11,2021)).
+file(notes1, docx, 757, date(25,10,2023)).
+file(report1, pdf, 99, date(25,10,2023)).
+file(notes2, docx, 77, date(25,10,2023)).
+file(report2, pdf, 252, date(25,10,2023)).
+
 % ---------- Containment facts ----------
 contains(root3, proj3_1).
 contains(root3, proj3_2).
@@ -35,3 +46,4 @@ contains(subpr3_2_4, cat3_2_4_2).
 contains(subpr3_2_4, cat3_2_4_3).
 contains(cat3_2_4_2, subcat3_2_4_2_1).
 contains(proj3_3, subpr3_3_1).
+
